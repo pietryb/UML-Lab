10c16e075da7b1a4e93c65a2c3f30fef426a5904
pietryb <brandon.pietryka@gmail.com>
2021-10-14 09:03:11 -0400

    first

---
+++ ClassUML/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassUML
+{
+    class Student : Person
+    {
+        public string program { get; set; }
+
+        public int year { get; set; }
+
+        public Student(string name, string address, string program, int year, double fee) : base(name, address)
+        {
+            this.program = program;
+            this.year = year;
+            this.fee = fee;
+
+        }
+        public Student() : base() {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Program: {program} Year {year} Fee {fee}";
+        }
+
+
+    }
+
+}
+
+
+    
+
+
+
---
+++ ClassUML/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassUML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Student("Charles", "123 Fake St.", "Business Ethics", 2022, 34000);
+            new Student("Bart", "742 Evergreen Terrace", "Nuclear Physics", 2023, 69000);
+            new Student("Dennis", "228 Race St.", "Milksteak Studies", 2022, 22000);
+            new Student("Michelle", "1530 Cameron Rd.", "Psycology", 2023, 46000);
+            new Student("Milhouse", "316 Pikeland Ave..", "Underwater Basket Weaving", 2024, 16000);
+
+        }
+
+       
+        
+    }
+}
